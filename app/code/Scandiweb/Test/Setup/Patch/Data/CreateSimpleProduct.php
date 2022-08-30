85bbe6b7fd7a50f9465f4031adde78bee8aa2692
nospyrin73 <mohammedzuhair527@gmail.com>
2022-08-30 17:28:03 +0200

    Code fixes

--- app/code/Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
+++ app/code/Scandiweb/Test/Setup/Patch/Data/CreateSimpleProduct.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Scandiweb\Test\Setup\Patch\Data;
 
+use Magento\Framework\Setup\Patch\DataPatchInterface;
 use Magento\Framework\App\State;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
@@ -16,7 +17,7 @@ use Magento\InventoryApi\Api\Data\SourceItemInterface;
 use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
 use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 
-class CreateSimpleProduct
+class CreateSimpleProduct implements DataPatchInterface
 {
     protected State $state;
 
@@ -44,6 +45,7 @@ class CreateSimpleProduct
     {
         $this->state = $state;
         $this->productInterfaceFactory = $productFactory;
+        $this->productRepository = $productRepository;
         $this->eavSetup = $eavSetup;
         $this->sourceItemFactory = $sourceItemFactory;
         $this->sourceItemsSave = $sourceItemsSave;
@@ -74,7 +76,8 @@ class CreateSimpleProduct
             ->setUrlKey('rtx3080')
             ->setPrice(699.00)
             ->setVisibility(Visibility::VISIBILITY_BOTH)
-            ->setStatus(Status::STATUS_ENABLED);
+            ->setStatus(Status::STATUS_ENABLED)
+            ->setAttributeSetId($attributeSetId);
 
         // Save the product
         $product = $this->productRepository->save($product);
@@ -89,5 +92,13 @@ class CreateSimpleProduct
         $this->sourceItems[] = $sourceItem;
 
         $this->sourceItemsSave->execute($this->sourceItems);
+    }
+
+    public function getAliases(): array {
+        return [];
+    }
+
+    public static function getDependencies(): array {
+        return [];
     }
 }
